85d4d8b7994496dca84b741fbdea1ced3e78e084
HiSandeep <vssandeep32@gmail.com>
2021-10-28 10:14:20 +0530

    regex example program added

---
+++ s2.py
@@ -0,0 +1,29 @@
+import re
+s = 1
+def inpname():
+    name=input("Hi,Enter your name:")#.lower()
+    #check=re.search("^[h]{1}[a]{1}[r]{1}[d]{1}",name)
+    check = re.search("^[a-zA-Z]{3,}$", name)
+    #print (check)
+    return check,name
+check1,name=inpname()
+
+def inpmobile():
+    mobile=input("Hi,Enter your mobile number:")#.lower()
+    #check=re.search("^[h]{1}[a]{1}[r]{1}[d]{1}",name)
+    check = re.search("^[6-9][0-9]{9}", mobile)
+    #print (check)
+    return check,mobile
+check2,number=inpmobile()
+
+while(s):
+    if (check1!=None and check2!=None):
+        print(f" Hi {name},\n Your mobile number is {number}")
+        s=0
+    else:
+        if check1==None:
+            print("please Enter your name correctly")
+            check1, name = inpname()
+        else :
+            print("please Enter your mobile number correctly")
+            check2, number = inpmobile()
